25eca5f20bd85af09a817a14efdabe1d44e77073
Dario Sechi <dario.sechi.rnd@gmail.com>
2023-12-25 23:37:05 +0100

    Stop using Environment class

--- src/hooks/use-safe-ref.ts
+++ src/hooks/use-safe-ref.ts
@@ -1,4 +1,4 @@
-import { Environment } from '@aracna/core'
+import { isWindowDefined } from '@aracna/core'
 import { MutableRefObject, useRef } from 'react'
 
 export function useSafeRef<K extends keyof HTMLElementTagNameMap>(tagName: K, options?: ElementCreationOptions): MutableRefObject<HTMLElementTagNameMap[K]>
@@ -7,5 +7,5 @@ export function useSafeRef<K1 extends keyof HTMLElementTagNameMap, K2 extends ke
   tagName: K1 | K2,
   options?: ElementCreationOptions
 ): MutableRefObject<HTMLElementTagNameMap[K1]> | MutableRefObject<SVGElementTagNameMap[K2]> {
-  return useRef(Environment.isWindowDefined ? document.createElement(tagName, options) : ({ current: {} } as any))
+  return useRef(isWindowDefined() ? document.createElement(tagName, options) : ({ current: {} } as any))
 }
